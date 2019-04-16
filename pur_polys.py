62d8ccabc1e8b86945273f617f91b58524a784e7
Raúl <rsotoalbors@gmail.com>
2019-04-16 17:00:52 -0400

    General cleanup; added ancestry deconvolution script

--- pur_polys.py
+++ pur_polys.py
@@ -82,24 +82,6 @@ pos = chr['variants/POS'].value
 gt = chr['calldata/GT']
 all_inds = chr['samples']
 
-# load allele frequencies for each population
-# afr_af = np.array(chr['variants/AFR_AF'], dtype=np.float64)
-# amr_af = np.array(chr['variants/AMR_AF'], dtype=np.float64)
-# sas_af = np.array(chr['variants/SAS_AF'], dtype=np.float64)
-# eas_af = np.array(chr['variants/EAS_AF'], dtype=np.float64)
-# eur_af = np.array(chr['variants/EUR_AF'], dtype=np.float64)
-
-# create empty arrays
-# sum_af = np.zeros([np.size(afr_af[:,1]),1], dtype=np.float64)   # sum of AF
-# af_ind = np.zeros([np.size(afr_af[:,1]),1], dtype=np.float64)      # retain original indices
-
-# concat population arrays, excluding AMR
-# no_amr_af = np.concatenate((af_ind, afr_af, sas_af, eas_af, eur_af), axis=1)
-
-# set maximum sum of AF threshold
-# if the sum exceeds thresh, we will not consider that SNP
-# thresh = 0                 # count: 1103547
-
 # read PUR individual list from tsv file (found on 1000Genomes)
 PUR_ids = np.genfromtxt(DEST_PATH+os.sep+"PUR_igsr_samples.tsv", dtype=str, delimiter='\t')[1:,0]
 
@@ -143,15 +125,12 @@ for i, snp in enumerate(snp_id):
         count = 0
     count += 1
     if np.any(np.nansum(PUR_inds[i,:,:], axis = 0)) > 0 and np.any(np.nansum(nonPUR_inds[i,:,:], axis = 0)) == 0:
-        # tmpPUR_snps.append(tuple([i,snp]))
         tmpPUR_snps.append(snp)
         snp_id_cp[1,i] = 1
     elif np.any(np.nansum(PUR_inds[i,:,:], axis = 0)) < 208 and np.any(np.nansum(nonPUR_inds[i,:,:], axis = 0)) == 2400:
-        # tmpPUR_snps.append(tuple([i,snp]))
         tmpPUR_snps.append(snp)
         snp_id_cp[1,i] = 1
     else:
-        # nonPUR_snps.append(tuple([i,snp]))
         nonPUR_snps.append(snp)
 
 tmpPUR_snps = set(tmpPUR_snps)
@@ -172,13 +151,6 @@ for j,prsnp in enumerate(tmpPUR_snps):
 with open(DEST_PATH+'chr'+str(chrom)+'PUR_SNPs.txt', 'w') as ff:
    ff.write(str(PUR_snps))
 
-#else:
-#    print()
-#    print("Loading PUR SNPs")
-#    PUR_snps = []
-#    with open(DEST_PATH+'chr'+str(chrom)+'PUR_SNPs.txt', 'r') as ff:
-#        PUR_snps = ast.literal_eval(ff.read())
-
 print()
 print("PUR SNPs found:\t"+str(len(PUR_snps)))
 print("Which is "+str(round((len(PUR_snps)/len(snp_id)*100), 2))+"% of all SNPs")
@@ -191,35 +163,6 @@ end = time.time()
 print("Elapsed time: "+str(round((end-start)/60,2)))
 print("----")
 print()
-
-# MEETING COMMENTS (OCT 4):
-# # 0 is usually more frequent than 1
-# # if alternate allele is not present
-# # Take files and eliminate all SNPs with more than 1 alternate allele
-# # For cases where most people are 1|1
-# # Ideas:
-# # # In PUR: sum > 0; in world = 0
-# # # 1) If any genotype has allele > 1; eliminate it
-# # # 2) Eliminate all SNPS where not (see up)
-# # See distribution of all SNPs along the chromosome
-# # One SNP every 100,000 bases
-# # We expect the distance between one SNP and another to be 100,000 bases
-# CELSR1 gene ancestry probably wrong
-#
-# NEXT:
-# # Identify distance between SNPs
-# # Reverse deconvolution of ancestry data
-# # Do our own deconvolution to see if it matches 1000Genomes' deconvolution
-# # # If our model coincides with theirs, confidence is improved
-# # # Should be different from 1000Genomes'
-# Deconvolution Ideas
-# # Eliminate ASW (they're African American; might bias our results)
-# # # ASW was analyzed to calculate AFR AF
-# # Isolate all EUR and AFR SNPs (similar to PUR SNP isolation)
-# # # We expect 1 SNP for every 10,000 bases
-# # # If there are 3 EUR SNPs where there are PUR SNPs, that area might be of EUR origin
-# # # # We should look at the area around the PUR SNPS
-# # # Based only on the unique SNPs for AFR and EUR
 
 f.flush()
 f.close()
--- read_inds.py
+++ read_inds.py
@@ -1,9 +1,10 @@
-import os
-import io
-import ast
-import os.path
+def list_inds(chrom):
+    import os
+    import io
+    import ast
+    import os.path
 
-chrom = 22
+    with open('D:/UPRM/GENOMES/chr'+str(chrom)+'_PUR_inds.txt', 'r') as ff:
+       PUR_inds = [line.rstrip('\n') for line in ff]
 
-with open('/home/raul/Documents/genomes/data/chr'+str(chrom)+'_PUR_inds.txt', 'r') as ff:
-   PUR_inds = [line.rstrip('\n') for line in ff]
+    return(PUR_inds)
